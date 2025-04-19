3571de0ad2f1f7594ad3ea9da2ca01fa07caeb6e
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-20 03:17:40 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // add any other paths where you use Tailwind CSS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
