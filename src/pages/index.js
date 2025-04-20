ea47a89461a74c97c5e296a748636f2571392823
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-21 01:22:28 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+// src/pages/index.js
+import '../styles/home.css'
+
+export default function Home() {
+  return (
+    <div>
+      <div className="logo">MailMind</div>
+      <h1>Welcome to MailMind</h1>
+      <p>Your smart inbox assistant to keep your emails focused and clutter-free.</p>
+    </div>
+  )
+}
