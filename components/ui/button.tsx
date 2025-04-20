c58ad7a31ccb0be1933d7ce306ee49f75e108731
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-21 00:08:02 +0530

    Delete components/ui directory

--- components/ui/button.tsx
+++
@@ -1,8 +0,0 @@
-export function Button({ className = "", ...props }) {
-  return (
-    <button
-      className={`transition duration-300 hover:opacity-80 ${className}`}
-      {...props}
-    />
-  );
-}
